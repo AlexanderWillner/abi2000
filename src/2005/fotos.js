70c71466cacb307ee9324f91be332b9c3df4321c
Alexander Willner <alex@willner.ws>
2011-05-10 00:30:00 +0200

    fixed scroll issue in 2005

--- src/2005/fotos.js
+++ src/2005/fotos.js
@@ -1,8 +1,8 @@
 _2005.Fotos = new Ext.TabPanel({
-    scroll: 'vertical',
     sortable: true,
     ui: 'dark',
     items: [{
+        scroll: 'vertical',
         title: 'Von Alex',
         html: 'Bitte das Bild anklicken...<br/><a href="http://abi2000.alexanderwillner.de/album/"><img class="album" src="./external/2005/fotos/alex/album_2005_alex.jpg" /></a>',
         cls: 'card'
