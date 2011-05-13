a5a7d0f44f8ba12b3e0a9203337ffe8cba304a5d
Alexander Willner <alex@willner.ws>
2011-05-13 23:12:30 +0200

    generate better zip

--- resources/php/getimages.php
+++ resources/php/getimages.php
@@ -4,13 +4,60 @@
 
 require ("pclzip.lib.php");
 
+function downloadFile( $fullPath ){
+  // Must be fresh start
+  if( headers_sent() )
+    die('Headers Sent');
+
+  // Required for some browsers
+  if(ini_get('zlib.output_compression'))
+    ini_set('zlib.output_compression', 'Off');
+
+  // File Exists?
+  if( file_exists($fullPath) ){
+
+    // Parse Info / Get Extension
+    $fsize = filesize($fullPath);
+    $path_parts = pathinfo($fullPath);
+    $ext = strtolower($path_parts["extension"]);
+
+    // Determine Content Type
+    switch ($ext) {
+      case "pdf": $ctype="application/pdf"; break;
+      case "exe": $ctype="application/octet-stream"; break;
+      case "zip": $ctype="application/zip"; break;
+      case "doc": $ctype="application/msword"; break;
+      case "xls": $ctype="application/vnd.ms-excel"; break;
+      case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
+      case "gif": $ctype="image/gif"; break;
+      case "png": $ctype="image/png"; break;
+      case "jpeg":
+      case "jpg": $ctype="image/jpg"; break;
+      default: $ctype="application/force-download";
+    }
+
+    header("Pragma: public"); // required
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Cache-Control: private",false); // required for certain browsers
+    header("Content-Type: $ctype");
+    header("Content-Disposition: attachment; filename=\"abi2000.zip\";" );
+    header("Content-Transfer-Encoding: binary");
+    header("Content-Length: ".$fsize);
+    ob_clean();
+    flush();
+    readfile( $fullPath );
+  } else
+    die('File Not Found');
+}
+
+
 $temp_file = tempnam(sys_get_temp_dir(), 'abi2000');
 $zipfile = new PclZip($temp_file);
 $v_list = $zipfile->create(array('../../external/2005/', '../../external/2010/'));
 if ($v_list == 0) {
     die ("Error: " . $zipfile->errorInfo(true));
 }
-header("Content-type: application/octet-stream");
-header("Content-disposition: attachment; filename=abi2000.zip");
-readfile($temp_file);
+
+downloadFile($temp_file);
 ?>
